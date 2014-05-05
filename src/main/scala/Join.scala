fa1e2e5edbf8679643fe7422f42b6081c1f545ab
Alon Dolev <alon@dolev.ch>
2014-05-05 20:34:52 +0200

    Working Sample: Object extends Pattern[A]
    This version type-checks. It's a little bit cumbersome though: For
    every future we need to create an object which extends the Pattern[A]
    class.

--- src/main/scala/Join.scala
+++ src/main/scala/Join.scala
@@ -2,26 +2,31 @@ import scala.concurrent.{ExecutionContext, Future}
 import ExecutionContext.Implicits.global
 
 object Join {
-  class Pattern[A](f: Future[A])
-  object Pattern {
-    def unapply[A](x: Pattern[A]): Option[A] = ???
+  class Pattern[A](f: Future[A]) {
+    def unapply(x: Pattern[A]): A = ???
+    def get: A = ???
+    def isEmpty: Boolean = ???
   }
   object && {
-    def unapply[A, B](a: Pattern[A], b: Pattern[B]): Option[(A, B)] = ???
+    def unapply[A, B](a: Pattern[A], b: Pattern[B]): Pattern[(A, B)] = ???
   }
-  def join[A](pf: PartialFunction[Any, A]): Future[A] = ???
+  def join[A](pf: PartialFunction[Pattern[A], A]): Future[A] = ???
 }
 
 object test extends App {
 
-  def await[A](f: Future[A]): A = ???
-
   import Join._
   val f = Future { 42 }
-  val g = Future { 10 }
+  val g = Future { 43 }
+  val h = Future { 44 }
+
+  object f1 extends Pattern(f)
+  object f2 extends Pattern(g)
+  object f3 extends Pattern(h)
 
   join {
-    case `f`(x) => x // does not work
+    case f1(x) && f2(y) => x + y
+    case f3(x) => x
   }
 
   /*
@@ -37,7 +42,8 @@ object test extends App {
     that "Something" (in that case) has to be be a "Stable Identifier",
     which is a "Path" with "Id" at the end.
 
-    We could force the user to create objects that extend Pattern.
+    We could force the user to create objects that extend Pattern, which type-checks,
+    but is not particularly nice.
 
    */
 }
