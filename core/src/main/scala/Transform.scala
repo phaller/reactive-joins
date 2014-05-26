6327a72b5db5237a7c85ece22f792c369894a09b
Alon Dolev <alon@dolev.ch>
2014-05-26 22:12:23 +0200

    Adds a more exapnend version of the transform

--- core/src/main/scala/Transform.scala
+++ core/src/main/scala/Transform.scala
@@ -2,7 +2,7 @@ import rx.lang.scala.Observable
 import rx.lang.scala.Subscription
 
 import concurrent.duration._
-import scala.collection.mutable
+import scala.collection.immutable.Queue
 import scala.concurrent.Future
 
 /* Example source of transform:
@@ -14,73 +14,96 @@ import scala.concurrent.Future
 *   println(result)
 * }
 * */
+
+class BitField(val value: Int) {
+  def set(mask: Int) = new BitField(value | mask)
+  def clear(mask: Int) = new BitField(value & ~mask)
+  def matches(mask: Int): Boolean = (~value & mask) == 0
+  def isSet(mask: Int): Boolean = (value & mask) == mask
+}
+object BitField {
+  def empty = new BitField(0)
+}
+
 object Transform extends App {
 
   val obs1 = Observable.interval(200 millis)
   val obs2 = Observable.interval(400 millis)
   val obs3 = Observable.interval(100 millis)
 
-  val observables = List(obs1, obs2, obs3)
-  val subscriptions = observables.map { obs =>
-    obs.subscribe(msg => resolveMessage(obs, msg))
-  }
+  val obs1_id = 1 << 0
+  val obs2_id = 1 << 1
+  val obs3_id = 1 << 2
 
-  val channels = new mutable.AnyRefMap[Observable[Any], mutable.Queue[Any]]()
+  // TODO: The Any type should correspond to the Observable type
+  var channels = Map[Int, Queue[Any]]()
 
-  var ids = new mutable.AnyRefMap[Observable[Any], Int]()
-  observables.zipWithIndex.foreach {
-    case (obs, index) => ids.put(obs, 1 << index)
-  }
+  val pattern1 = obs1_id | obs2_id
+  val pattern2 = obs3_id
 
-  def getId[A](obs: Observable[A]): Int = ids.getOrElse(obs, throw Exception)
-
-  class BitField(val value: Int) {
-    def set(mask: Int) = new BitField(value | mask)
-    def clear(mask: Int) = new BitField(value & ~mask)
-    def matches(mask: Int): Boolean = (~value & mask) == 0
-    def isSet(mask: Int): Boolean = (value & mask) == mask
-  }
-  object BitField {
-    def empty = new BitField(0)
-  }
-
-  class Pattern[A, B](val target: Int, val body: (A => B)) extends BitField(target)
-  object Pattern {
-    def apply[A, B](target: Int, body: (A => B)) = new Pattern(target, body)
-  }
-
-  val patterns = List(
-    Pattern(getId(obs1) | getId(obs2), { case (x, y) => x + y }),
-    Pattern(getId(obs3), { case (x) => x })
-  )
+  val pattern1_continuation = (v1: Long, v2: Long) => v1 + v2
+  val pattern2_continuation = (v1: Long) => v1
 
   val continuation: (Any => Unit) = ???
 
   var state = BitField.empty
 
-  def resolveMessage[A](sender: Observable[A], msg: A) = {
-    patterns.synchronized {
-      val senderId = getId(sender)
-      state.set(senderId)
-      patterns.find(p => p.matches(state.value)) match {
-        case Some(p) => {
-          state.clear(p.value)
-          // How to map the arguments to the body?
-          val queues = channels.map({ case (obs, _) => getId(obs) })
-                               .filter({ case (obs, _) => p.isSet(obs) })
-                               .toList
-          Future {
-              val result = p.body(???)
-              continuation(result)
+  obs1.subscribe(
+    next => {
+      state.synchronized {
+        if (state.set(obs1_id).matches(pattern1)) {
+          val obs2_value = channels.get(obs2_id) match {
+            case Some(x: Long) => x
           }
+          Future {
+            val result = pattern1_continuation(next, obs2_value)
+            continuation(result)
+          }
+        } else {
+          channels.get(obs1_id) match {
+            case Some(q) => q.enqueue(next)
+            case None => channels += (obs1_id -> Queue(next))
+          }
+          state.set(obs1_id)
         }
-        case None => {
-          channels.get(sender) match {
-            case Some(c) => c += msg
-            case None => channels += (sender -> mutable.Queue(msg))
+      }
+    }
+  )
+
+  obs2.subscribe(
+    next => {
+      state.synchronized {
+        if (state.set(obs2_id).matches(pattern1)) {
+          val obs1_value = channels.get(obs1_id) match {
+            case Some(x: Long) => x
+          }
+          Future {
+            val result = pattern1_continuation(obs1_value, next)
+            continuation(result)
+          }
+        } else {
+          channels.get(obs2_id) match {
+            case Some(q) => q.enqueue(next)
+            case None => channels += (obs2_id -> Queue(next))
+          }
+          state.set(obs2_id)
+        }
+      }
+    }
+  )
+
+  obs3.subscribe(
+    next => {
+      state.synchronized {
+        if (state.set(obs3_id).matches(pattern2)) {
+          Future {
+            val result = pattern2_continuation(next)
+            continuation(result)
           }
         }
       }
     }
-  }
+  )
+
+
 }
