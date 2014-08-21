5f5fa5a6fce751dd8f939a1ed9a08bc14fadfd14
Alon Dolev <alon@dolev.ch>
2014-08-21 21:59:07 +0200

    Adds more tests

--- src/test/scala/scala/async/run/SimpleJoin.scala
+++ src/test/scala/scala/async/run/SimpleJoin.scala
@@ -3,54 +3,77 @@ package scala.async
 import scala.async.Join._
 import org.junit.Test
 import rx.lang.scala.Observable
+import rx.lang.scala.schedulers.TestScheduler
 import scala.concurrent.duration._
 
 class AsyncSpec {
 
   @Test
   def `unary join`() = {
-    val o1 = Observable.items(1).p
+    val input = (1 to 10).toList
+    val fn = (x: Int) => x + 1
+    val expected = input.map(fn)
+
+    val o1 = Observable.items(input:_*).p
     
-    val obs: Observable[Int] = join {
-      case o1(x) => Some(x + 1)
+    val obs = join {
+      case o1(x) => Some(fn(x))
       case o1.done => None
     }
 
-    assert(obs.map(x => x == 2).toBlocking.first)
+    val result = obs.toBlocking.toList
+    
+    assert(result == expected)
   }
 
-  // @Test
-  // def `unary join error`() = {
-  //   val o1 = Observable.items(1).p
+  @Test
+  def `unary join error`() = {
+    val input = (1 to 10).toList
+    val o1 = Observable.items(input: _*).map(_ => throw new Exception("")).p
     
-  //   val obs = join {
-  //     case o1.error(e) => Some(e)
-  //   }
+    val obs = join {
+      case o1.error(e) => Some(e)
+    }
 
-  //   assert(obs.map(x => x == 2).toBlocking.first)
-  // }
+    val result = obs.map({ case e: Exception => true }).toBlocking
 
-  // @Test
-  // def `unary join done`() = {
-  //   val o1 = Observable.items(1).p
+    assert(result.first)
+  }
+
+  @Test
+  def `unary join done`() = {
+    val input = (1 to 10).toList
+    val output = 1
+    val o1 = Observable.items(input: _*).p
     
-  //   val obs = join {
-  //     case o1.done => Some(2)
-  //   }
-
-  //   assert(obs.map(x => x == 2).toBlocking.first)
-  // }
-
-  // @Test
-  // def `binary join`() = {
-  //   val o1 = Observable.items(1).p
-  //   val o2 = Observable.items(1).p
+    val obs = join {
+      case o1.done => Some(output)
+    }
     
-  //   val obs = join {
-  //     case o1(x) && o2(y) => Some(x + y)
-  //   }
+    val result = obs.map(x => x == 1).toBlocking
 
-  //   assert(obs.map(x => x == 2).toBlocking.first)
-  // }
+    assert(result.first)
+  }
+
+  @Test
+  def `binary or join`() = {
+    val scheduler1 = TestScheduler()
+    val scheduler2 = TestScheduler()
+
+    val o1 = Observable.items(1).subscribeOn(scheduler1).p
+    val o2 = Observable.items(2).subscribeOn(scheduler2).p
+    
+    val obs = join {
+      case o1(x) => Some(x)
+      case o2(y) => Some(y)
+      case o1.done && o2.done => None
+    }
+
+    scheduler2.triggerActions()
+    scheduler1.triggerActions()
+
+    val result = obs.toBlocking.toList
+    assert(result == List(2, 1))
+  }
 
 }
