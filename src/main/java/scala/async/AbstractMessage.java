ea66791f59dc5a5241f40d42c5df96268220a2a0
Alon Dolev <alon@dolev.ch>
2014-09-26 16:37:26 +0200

    Adds comments to AbstractMessage

--- src/main/java/scala/async/AbstractMessage.java
+++ src/main/java/scala/async/AbstractMessage.java
@@ -1,4 +1,5 @@
 // Source: https://github.com/scala/scala/blob/2.11.x/src/library/scala/concurrent/impl/AbstractPromise.java
+// We need to use Java because the Unsafe.CAS requires static fields which do not exist in Scala.
 package scala.async;
 
 import scala.concurrent.util.Unsafe;
@@ -6,6 +7,7 @@ import scala.concurrent.util.Unsafe;
 abstract class AbstractMessage {
   private volatile Object _ref;
   final static long _refoffset;
+  // Static initializer: executed when the class is loaded.
   static {
     try {
       _refoffset = Unsafe.instance.objectFieldOffset(AbstractMessage.class.getDeclaredField("_ref"));
